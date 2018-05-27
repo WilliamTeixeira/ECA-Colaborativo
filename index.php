71f32c65ef5f20d0468563b116efa5c3f46b2b22
WilliamTeixeira <tx.william@gmail.com>
2018-05-27 11:43:49 -0300

    Primero commit:  - Cruds  - Login  - Recupera senha

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Description of subfunctionsDAO
+ *
+ * @author wtx
+ */
+
+require_once "classes/template.php";
+
+$template = new Template();
+
+$template->header();
+$template->sidebar();
+$template->mainpanel();
+$logado = $_SESSION["name"]
+?>
+
+<div class='content' xmlns="http://www.w3.org/1999/html">
+    <div class='container-fluid'>
+        <div class='row'>
+            <div class='col-md-12'>
+                <div class='card'>
+                    <div class='header'>
+                        <h1 class='title' style="text-align: center">Sistema EconomiC Analyzer</h1>
+                        <p class='category'></p>
+                    </div>
+                    <div class='content table-responsive'>
+                          <h4 class="title">Bem-vindo ao sistema <?=$logado;?> </h4>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+$template->footer();
+?>
---
+++ relatorio/aluno_relatorio.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tassio
+ * Date: 05/01/2018
+ * Time: 18:00
+ */
+
+//So funciona se desativar os erros!
+ini_set('display_errors', 1);
+
+//include("../libs/mpdf/mpdf.php");
+require_once "../libs/mpdf/mpdf.php";
+require_once "../db/alunoDAO.php";
+
+$alunoDAO = new alunoDAO();
+
+$listObjs = $alunoDAO->findAll();
+
+foreach ($listObjs as $var):
+    $html.=$var->Nome.='<br>';
+endforeach;
+
+$mpdf=new mPDF();
+$mpdf->SetCreator(PDF_CREATOR);
+$mpdf->SetAuthor('Tassio Sirqueira');
+$mpdf->SetTitle('TCPDF Exemplo');
+$mpdf->SetSubject('TCPDF Aula');
+$mpdf->SetKeywords('TCPDF, PDF, exemplo');
+$mpdf->SetDisplayMode('fullpage');
+$mpdf->nbpgPrefix = ' de ';
+$mpdf->setFooter('Página {PAGENO}{nbpg}');
+$mpdf->WriteHTML($html);
+$mpdf->Output('Exemplo.pdf','I');
+
+exit;
