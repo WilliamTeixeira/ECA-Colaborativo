029ec1bf2a2bc7e6ab4b917b3ceff2d3aa092ee4
Hugo Nogueira <hnogjf@gmail.com>
2018-05-31 18:08:28 -0300

    Update relatorioGeral.php
    Erro encontrado e corrigido

--- relatorioGeral.php
+++ relatorioGeral.php
@@ -39,11 +39,14 @@ $template->mainpanel();
                         </form>
                         
                         <?php
+                        if (isset($_POST['relatoriosDisponiveis'])){
                         $relatorioselecionado = $_POST['relatoriosDisponiveis'];
                         if ($relatorioselecionado=="relatorioNulo"){ 
                            echo "Por favor selecione um relatorio da lista a cima!";
-                        }else { 
+                        }else if ($relatorioselecionado=="relatorio01"){ 
                              echo "<script>script:window.open('http://localhost/PHP/ECA-Colaborativo/relatorio/".$relatorioselecionado.".php', '_blank');</script>";
+                        }
+                        
                         }
                         echo (isset($msg) && ($msg != null || $msg != "")) ? $msg : '';
                         ?>
